8f8e686b0001f4e775d58cabb10bfcc1ee62d18c
Hector Fernandez <hectorj@gmail.com>
2017-03-10 13:30:58 +0100

    Installed the certificate into the system keystore and created a custom keystore

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Import the certificate
-cp /opt/jboss/InstallCert/* .
+cd /opt/jboss/keycloak/InstallCert/
 java InstallCert $OSO_ADDRESS << ANSWERS
 1
 ANSWERS
@@ -10,11 +10,40 @@ if [[ -z "${KEYSTORE_PASSWORD}" ]]; then
   KEYSTORE_PASSWORD="almighty"
 fi
 
-echo "Setting a password to the new keystore..."
-keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore jssecacerts -storepass changeit
+if [[ -z "${KEYCLOAK_SERVER_DOMAIN}" ]]; then
+  KEYCLOAK_SERVER_DOMAIN="localhost"
+fi
 
-echo "Exporting the certificate into the keystore..."
-keytool -exportcert -alias $OSO_DOMAIN_NAME-1 -keystore jssecacerts -storepass changeit -file openshift-io.cer
+echo "Export the certificate into the keystore"
+keytool -exportcert -alias $OSO_DOMAIN_NAME-1 -keystore jssecacerts -storepass changeit -file $OSO_DOMAIN_NAME.cer
+
+echo "Import certificate into system keystore"
+keytool -importcert -alias $OSO_DOMAIN_NAME-1 -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -file $OSO_DOMAIN_NAME.cer << ANSWERS
+yes
+ANSWERS
+
+echo "Create keycloak store"
+keytool -genkey -alias ${KEYCLOAK_SERVER_DOMAIN} -keyalg RSA -keystore keycloak.jks -validity 10950 -keypass $KEYSTORE_PASSWORD -storepass $KEYSTORE_PASSWORD << ANSWERS
+${KEYCLOAK_SERVER_DOMAIN}
+Keycloak
+Red Hat
+Westford
+MA
+US
+yes
+ANSWERS
+
+echo "Import certificate to keycloak store"
+keytool -importcert -alias $OSO_DOMAIN_NAME-1 -keystore keycloak.jks -storepass ${KEYSTORE_PASSWORD} -file $OSO_DOMAIN_NAME.cer << ANSWERS
+yes
+ANSWERS
+
+mv keycloak.jks ../standalone/configuration
+
+cd /opt/jboss/keycloak
+
+# Set the password of the keystore to the configuration file
+sed -i -e "s/%%KEYSTORE_PASSWORD%%/${KEYSTORE_PASSWORD}/" ./standalone/configuration/standalone.xml
 
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     echo "Adding a new user..."

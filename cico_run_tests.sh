ac44896095a33b428aae0d5b9641b11b3faa9af2
Hector Fernandez <hectorj@gmail.com>
2017-03-27 20:49:35 +0200

    Build only the keycloak-server

--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -3,8 +3,8 @@
 . cico_setup.sh
 
 function run_tests() {
-  echo 'CICO: Run mv clean install -Pdistribution'
-  mvn clean install -Pdistribution
+  echo 'CICO: Run mv clean install -pl :keycloak-server-dist -am -Pdistribution'
+  mvn clean install -pl :keycloak-server-dist -am -Pdistribution
 
   echo 'CICO: keycloak-server tests completed successfully!'
 }
